c3ea4d7d04eae75490f75ccb907940c26948ee0b
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-02-15 15:02:20 +0530

    adding habit tracker
    adding new project using redux

---
+++ habit-tracker-app/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Habits from './Habits'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar name="View Details"/>
+      <Habits/>
+    </>
+  )
+}
+
+export default Home
